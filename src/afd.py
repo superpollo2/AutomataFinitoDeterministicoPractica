395646d5d665de258f8390e0e51b817cb547a572
laura tascon <laura.tascon@udea.edu.co>
2023-10-10 22:15:35 -0500

    cambio en la configuración sonar

--- src/afd.py
+++ src/afd.py
@@ -1,6 +1,6 @@
 
 ESTADOS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
+from node import Node
 
 class DDFA:
     def __init__(self, tree, symbols, regex):
@@ -216,23 +216,4 @@ class DDFA:
     
 
 
-class Node:
-    def __init__(self, _id, firstpos=None, lastpos=None, nullable=False, value=None, c1=None, c2=None):
-        self._id = _id
-        self.firstpos = firstpos
-        self.lastpos = lastpos
-        self.followpos = list()
-        self.nullable = nullable
-        self.value = value
-        self.c1 = c1
-        self.c2 = c2
 
-    def __repr__(self):
-        return f'''
-    id: {self._id}
-    value: {self.value}
-    firstpos: {self.firstpos}
-    lastpos: {self.lastpos}
-    followpos: {self.followpos}
-    nullabe: {self.nullable}
-    '''
---
+++ src/node.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, _id, firstpos=None, lastpos=None, nullable=False, value=None, c1=None, c2=None):
+        self._id = _id
+        self.firstpos = firstpos
+        self.lastpos = lastpos
+        self.followpos = list()
+        self.nullable = nullable
+        self.value = value
+        self.c1 = c1
+        self.c2 = c2
+
+    def __repr__(self):
+        return f'''
+    id: {self._id}
+    value: {self.value}
+    firstpos: {self.firstpos}
+    lastpos: {self.lastpos}
+    followpos: {self.followpos}
+    nullabe: {self.nullable}
+    '''
